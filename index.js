cc5eb319ee809de593f7b5a095c0dfa4eccab785
Aamir Saleem <aamirsaleem167@gmail.com>
2022-05-04 19:47:34 +0500

    add dispatch method

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
-function createStore() {
+function todos(state, action) {
+    if (action.type === 'ADD_TODO') {
+        return state.concat(action.todo);
+    }
+
+    return state;
+}
+
+function createStore(reducer) {
 
     let state;
     let listeners = [];
+
 
     const subscribe = (listener) => {
         listeners.push(listener);
@@ -11,11 +20,16 @@ function createStore() {
         }
     }
 
+    const dispatch = (action) => {
+        state = reducer(state, action);
+        listeners.forEach(listener => listener())
+    }
 
     const getState = () => state;
 
     return {
         getState,
+        dispatch,
         subscribe
     }
 }
