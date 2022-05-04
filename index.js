e070f1ae35578112cdc454143e932aef80ae5de0
Aamir Saleem <aamirsaleem167@gmail.com>
2022-05-04 14:29:21 +0500

    add listeners

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 function createStore() {
 
     let state;
+    let listeners = [];
+
+    const subscribe = (listener) => {
+        listeners.push(listener);
+
+        return () => {
+            listeners = listeners.filter(l => l !== listener);
+        }
+    }
+
 
     const getState = () => state;
 
     return {
-        getState
+        getState,
+        subscribe
     }
 }
